371ba97ef4b7d51f2752dc7cd39d21e010da66ee
Denis Brumann <denis.brumann@sensiolabs.de>
2018-10-13 14:43:42 +0200

    Adds login functionality.
    - Creates test user for login
    - Secures dashboard behind firewall

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
     /**
+     * @IsGranted("ROLE_USER")
      * @Route(path="/", name="dashboard")
      */
     public function index()
